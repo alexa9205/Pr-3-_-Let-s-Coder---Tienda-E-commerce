5a2fb6983f6fce924cf6360153abe5e658d751f9
Alexandra David <alexa929292@gmail.com>
2021-07-06 12:00:32 +0200

    heroku

--- server.js
+++ server.js
@@ -37,6 +37,15 @@ mongoose.connect(URI, {
 })
 
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
+
 
 
 const PORT = process.env.PORT || 5000
